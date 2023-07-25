86104ba52db5752be55cfc0c7f21a01c96d0eb3f
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-07-25 18:49:47 -0300

    Arquivos aula 01 python fundamentos

--- modulo01-fundamentos/20230724-aula01/prog03.py
+++ modulo01-fundamentos/20230724-aula01/prog03.py
@@ -45,3 +45,30 @@ if __name__ == "__main__":
 
     # A palavra reservada not representa o operador lógico NÃO
     print(not 5 > 1)
+
+    # Operadores de identidade (is, is not)
+    # Verificam se um objeto é igual
+
+    lista1 = ["Python", "C3"]
+    lista2 = lista1
+
+    print(lista1 is not lista2)
+    print(id(lista1), id(lista2))
+
+    # Operadores de pertencimento (in, not in)
+    # Verificam se um valor está contido dentro de uma
+    # sequência (ou container)
+    lista_usuarios = ["maria", "joao", "jose", "paulo"]
+
+    meu_usuario = "joao"
+
+    if meu_usuario in lista_usuarios:
+        print("Você está na lista. Acesso aprovado.")
+    else:
+        print("Você não está na lista. Acesso negado.")
+
+    # Podemos utilizar esses operadores em strings
+    # Strings são *sequências* de caracteres
+
+    if "Y" not in "Python".upper():
+        print("Existe a letra Y.")
