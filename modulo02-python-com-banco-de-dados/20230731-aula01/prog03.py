dceaeb78ad031c9f283d2a1764bb002a89efd6ef
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-08-01 20:55:25 -0300

    Arquivos aula 01 python com banco de dados.

--- modulo02-python-com-banco-de-dados/20230731-aula01/prog03.py
+++ modulo02-python-com-banco-de-dados/20230731-aula01/prog03.py
@@ -23,13 +23,12 @@ class Veiculo:
     def desligar(self):
         print(f"{self._marca} ({self._modelo}/{self._ano}) desligou.")
 
-
+"""
+A linha abaixo indica que a classe Carro está herdando os métodos e os atributos
+definidos na classe Veiculo. Podemos chamar a classe Carro de subclasse(ou classe filha)
+e a classe Veículo de superclasse (ou classe pai/mãe)
+"""
 class Carro(Veiculo):
-
-    def __init__(self, marca, modelo, ano):
-        self._marca = marca
-        self._modelo = modelo
-        self._ano = ano
 
     def abrir_portas(self):
         print("Portas do carro abertas")
@@ -38,31 +37,32 @@ class Carro(Veiculo):
         print("Portas do carro fechadas")
 
 
-class Moto:
+class Moto(Veiculo):
 
     def __init__(self, marca, modelo, ano, cilindradas):
-        self._marca = marca
-        self._modelo = modelo
-        self._ano = ano
+        """
+        A função built-in super() executa um método definido na
+        classe mãe. Usamos essa função pra executar um método que está sendo
+        herdado
+        """
+        super().__init__(marca=marca, modelo=modelo, ano=ano)
         self._cilindradas = cilindradas
-
-    def ligar(self):
-        print("Moto ligada")
-
-    def desligar(self):
-        print("Moto desligada")
 
     def dar_grau(self):
         print("Chama no grau")
 
 
-class Bicicleta:
+class Bicicleta(Veiculo):
 
     def __init__(self, marca, modelo, ano, numero_de_marchas):
-        self._marca = marca
-        self._modelo = modelo
-        self._ano = ano
+        super().__init__(marca=marca, modelo=modelo, ano=ano)
         self._numero_de_marchas = numero_de_marchas
+
+    def ligar(self):
+        raise Exception("Bicicleta não liga!")
+    
+    def desligar(self):
+        raise Exception("Bicicleta não desliga")
 
     def pedalar(self):
         print("Bicicleta pedalando")
@@ -73,3 +73,12 @@ if __name__ == "__main__":
     gol_quadrado.fechar_portas()
     gol_quadrado.ligar()
     gol_quadrado.desligar()
+
+    vespa = Moto("Vespa", "Vespa 1979", 1979, 50)
+    vespa.ligar()
+    vespa.dar_grau()
+    vespa.desligar()
+
+    bicicleta = Bicicleta("Specialized", "Specialized 200000", 2023, 29)
+    bicicleta.pedalar()
+    bicicleta.ligar()
