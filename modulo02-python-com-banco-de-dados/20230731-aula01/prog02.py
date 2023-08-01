5abdb855efa181a75a0e1afbad707802edb81d16
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-07-31 22:13:35 -0300

    Arquivos aula 01 Python com banco de dados

---
+++ modulo02-python-com-banco-de-dados/20230731-aula01/prog02.py
@@ -0,0 +1,13 @@
+"""
+Programação Orientada a Objetos
+
+Encapsulamento
+
+É o processo de "esconder" detalhes da implementação de uma classe de outros
+objetos. Esse processo é um pouco diferente no Python, se formos comparar com
+outras linguagens como Java e C#.
+
+"""
+
+class ContaBancaria:
+    pass
