913c48ae151bd26b9bd814f99d12d7349c8c8711
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-08-02 22:11:44 -0300

    Arquivos aula 03 python com banco de dados.

--- modulo02-python-com-banco-de-dados/20230802-aula03/3fn.sql
+++ modulo02-python-com-banco-de-dados/20230802-aula03/3fn.sql
@@ -4,6 +4,8 @@ não de outras colunas. Isso ajuda a evitar problemas de redundância e inconsis
 desnecessárias ou irrelevantes, e também ajuda a melhorar a eficiência de consultas ao banco de dados.
 */
 
+USE modulo02_python;
+
 -- tb_pedidos_itens
 
 CREATE TABLE IF NOT EXISTS tb_pedidos_itens(
@@ -11,7 +13,7 @@ CREATE TABLE IF NOT EXISTS tb_pedidos_itens(
 	item_id			INTEGER		NOT NULL,
 	valor_unitario	FLOAT		NOT NULL,
 	quantidade		INTEGER		NOT NULL,
-	subtotal		REAL		NOT NULL,
+	subtotal		FLOAT		NOT NULL,
 	PRIMARY KEY(pedido_id, item_id)
 );
 
@@ -22,3 +24,51 @@ INSERT INTO tb_pedidos_itens (pedido_id, item_id, valor_unitario, quantidade, su
 
 
 SELECT * FROM tb_pedidos_itens ;
+
+SELECT pedido_id, item_id, quantidade, subtotal FROM tb_pedidos_itens;
+
+RENAME TABLE tb_pedidos_itens TO tb_pedidos_itens_pre_3fn;
+
+CREATE TABLE IF NOT EXISTS tb_itens(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(200) NOT NULL,
+    valor_unitario FLOAT NOT NULL
+);
+
+INSERT INTO tb_itens(nome, valor_unitario) VALUES
+	("Garrafa Térmica", 50),
+    ("Caixa de som", 99),
+    ("Smartwatch", 199.90),
+    ("Máquina de cortar cabelo", 50);
+    
+SELECT * FROM tb_itens;
+
+CREATE TABLE IF NOT EXISTS tb_pedidos_itens(
+	pedido_id INT NOT NULL,
+    item_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    
+    PRIMARY KEY(pedido_id, item_id),
+    FOREIGN KEY(item_id) REFERENCES tb_itens(id)
+);
+
+INSERT INTO tb_pedidos_itens(pedido_id, item_id, quantidade) VALUES
+	(1, 1, 2),
+    (1, 2, 3),
+    (1, 4, 2),
+    (2, 2, 1),
+    (2, 4, 3);
+    
+CREATE VIEW vw_total_pedidos AS
+SELECT
+	tpi.pedido_id,
+    ti.nome,
+    ti.valor_unitario,
+    tpi.quantidade,
+    FORMAT(ti.valor_unitario * tpi.quantidade, 2) AS Subtotal
+FROM tb_itens ti
+INNER JOIN tb_pedidos_itens tpi
+ON ti.id = tpi.item_id
+ORDER BY Subtotal DESC;
+
+SELECT * FROM vw_total_pedidos;
