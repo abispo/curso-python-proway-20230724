c9120a66bdf7cf367fd304feb64952020599404b
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-08-07 20:27:48 -0300

    Arquivos aula 06 Python com banco de dados

--- modulo02-python-com-banco-de-dados/20230807-aula06/main.py
+++ modulo02-python-com-banco-de-dados/20230807-aula06/main.py
@@ -1,10 +1,43 @@
 # main.py
 
 from database import engine, Base
-
+from messages import MAIN_MENU
 from models import User, Profile, Post
+from users import list_users, create_user
 
 
 if __name__ == "__main__":
 
     Base.metadata.create_all(engine)
+
+    while True:
+
+        print(MAIN_MENU)
+
+        opcao = int(input("Informe a opção desejada: "))
+
+        match opcao:
+            case 0:
+                print("Encerrando...")
+                break
+            case 1:
+                
+                email = input("Informe o seu e-mail: ")
+                password = input("Informe uma senha: ")
+                first_name = input("Informe o seu primeiro nome: ")
+                last_name = input("Informe o seu sobrenome: ")
+
+                user = create_user(email, password, first_name, last_name)
+
+                print(f"Usuário {user.email} criado com sucesso!")
+
+            case 2:
+                users = list_users()
+
+                if len(users) == 0:
+                    print("Não existem usuários cadastrados.")
+
+            case 3:
+                pass
+            case 4:
+                pass
---
+++ modulo02-python-com-banco-de-dados/20230807-aula06/messages.py
@@ -0,0 +1,14 @@
+# messages.py
+
+
+MAIN_MENU = """
+ESCOLHA A OPÇÃO
+---------------
+
+0 - Sair
+1 - Criar Novo Usuário
+2 - Listar Usuários
+3 - Atualizar Usuário
+4 - Remover Usuário
+
+"""
