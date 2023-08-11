e4698b9e653b1819d911b7ea5b73c4615d66690b
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-08-11 19:53:39 -0300

    Arquivos aula 02 python web

--- modulo03-python-web/20230811-aula02/polls/models.py
+++ modulo03-python-web/20230811-aula02/polls/models.py
@@ -4,8 +4,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("Data de publicação")
 
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
--- modulo03-python-web/20230811-aula02/polls/views.py
+++ modulo03-python-web/20230811-aula02/polls/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Bem vindo ao site de enquetes. Essa é a página principal.")
+
+def detail(request, question_id):
+    return HttpResponse(f"Você está acessando a pergunta de id {question_id}.")
+
+def results(request, question_id):
+    return HttpResponse(f"Você está pa nágina de resultados da pergunta de id {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"Você está acessando a página de votação da pergunta de id {question_id}.")
