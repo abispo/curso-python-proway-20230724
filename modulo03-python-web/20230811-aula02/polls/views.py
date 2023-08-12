8accac5346c10523867c4669f61ec13ea4b46aca
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-08-11 22:11:37 -0300

    Arquivos aula 02 python web

--- modulo03-python-web/20230811-aula02/polls/views.py
+++ modulo03-python-web/20230811-aula02/polls/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from polls.models import Question
+
 def index(request):
-    return HttpResponse("Bem vindo ao site de enquetes. Essa é a página principal.")
+
+    # session.query(Question).order_by("-pub_date")
+    # SELECT * FROM polls_question ORDER BY pub_date DESC
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    context = {"latest_question_list": latest_question_list}
+
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse(f"Você está acessando a pergunta de id {question_id}.")
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     return HttpResponse(f"Você está na página de resultados da pergunta de id {question_id}.")
