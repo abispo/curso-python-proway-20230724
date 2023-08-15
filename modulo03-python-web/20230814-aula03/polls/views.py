8c628c5e32c03e3157881bcd5443bb3353715efe
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-08-14 22:03:52 -0300

    Arquivos aula 03 python web

--- modulo03-python-web/20230814-aula03/polls/views.py
+++ modulo03-python-web/20230814-aula03/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from polls.models import Question
+from polls.models import Question, Choice
 
 def index(request):
 
@@ -23,4 +24,22 @@ def results(request, question_id):
     return HttpResponse(f"Você está na página de resultados da pergunta de id {question_id}.")
 
 def vote(request, question_id):
-    return HttpResponse(f"Você está acessando a página de votação da pergunta de id {question_id}.")
+    
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    
+    except (KeyError, Choice.DoesNotExist):
+        context = {
+            "question": question,
+            "error_message": "Você deve escolher uma das opções"
+        }
+        return render(request, "polls/detail.html", context)
+    
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
