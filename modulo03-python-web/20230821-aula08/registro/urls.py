3f29192d0b4dc7c6fbf3f5448a651b36554fc67f
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-08-21 19:57:33 -0300

    Arquivos aula 08 python web

--- modulo03-python-web/20230821-aula08/registro/urls.py
+++ modulo03-python-web/20230821-aula08/registro/urls.py
@@ -7,5 +7,5 @@ app_name = "registro"
 urlpatterns = [
     path('pre-registro/', views.pre_registro, name="pre_registro"),
     path('envio-email-pre-registro/', views.envio_email_pre_registro, name="envio_email_pre_registro"),
-    path('confirmar-cadastro/', views.confirmar_cadastro, name="confirmar_cadastro")
+    path('confirmacao/', views.confirmar_cadastro, name="confirmar_cadastro")
 ]
