7e3ebc91460d8aed7fb62ff96488f17be11ca797
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-08-25 22:06:54 -0300

    Arquivos aula 12 python web

--- modulo03-python-web/20230825-aula12/hospedagem/urls.py
+++ modulo03-python-web/20230825-aula12/hospedagem/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('registro/', include('registro.urls')),
     path('hospedagem/', include('core.urls')),
     path('reservas/', include('reservas.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
